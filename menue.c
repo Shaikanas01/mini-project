3e2af52216e20505b76035a9b2e3a5ff2bf1c185
Shaikanas01 <shaikanas478@gmail.com>
2026-06-17 12:52:47 +0000

    first change

---
+++ menue.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+ int hello ,hi;
+
+char op;
+printf("----welcome to our hotel----\n\n");
+printf("select what you want-->\n");
+printf("1.starter\n2.main coursee\n3.desert\n4.exit\n");
+scanf("%c",&op);
+
+switch(op)
+{
+    case '1':
+    printf("you have choosen stater\n");
+    printf("1.chicken 65\n2.paneer fry\n3.dragon chicken");
+    break;
+
+    case '2':
+    printf("you have choosen main course\n");
+    printf("1.dum biryani\n2.tanduri roti\n3.veg biryani");
+    break;
+
+
+
+
+}
+
+
+
+
+}
